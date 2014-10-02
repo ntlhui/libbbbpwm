f2a54b9addb7e2bb9b91a9167ebc53a9e789756b
Nathan Hui <ntlhui@gmail.com>
2014-10-02 12:16:09 -0700

    Added pin defines.  Added includes section.  Added private state variables

--- src/PWM.hpp
+++ src/PWM.hpp
@@ -23,15 +23,42 @@
  * USA
  */
 
+// Includes
+#include <cstdlib>
+#include <assert.h>
+
+// Constants Definitions
+#define	PWM_P8_13	0
+#define PWM_P8_19	1
+#define PWM_P8_34	2
+#define PWM_P8_36	3
+#define PWM_P8_45	4
+#define PWM_P8_46	5
+#define PWM_P9_14	6
+#define PWM_P9_16	7
+#define PWM_P9_21	8
+#define PWM_P9_22	9
+#define PWM_P9_28	10
+#define PWM_P9_29	11
+#define PWM_P9_31	12
+#define PWM_P9_42	13
+
 namespace PWM{
 	class PWM{
 		public:
-			PWM(const uint8_t header, const uint8_t pin, const uint32_t frequency);
+			PWM(const uint8_t pin, const uint32_t frequency);
 			~PWM();
 			int setDuty(const float dutyPercentage);
 			int setPeriod(const uint32_t period);
 			int setPolarity(const bool polarity);
 			int setOnTime(const uint32_t onTime);
+			int setState(const bool running);
+		private:
+			uint8_t _pin;
+			uint8_t _period;
+			uint8_t _pulseWidth;
+			bool _polarity;
+			bool _running;
 	}
 }
 
