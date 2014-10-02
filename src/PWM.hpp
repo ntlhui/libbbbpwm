e308a43599d6f7bf47067b08a252f622972030e8
Nathan Hui <ntlhui@gmail.com>
2014-10-02 11:44:42 -0700

    Fixed spelling error in license section in top doc comment

--- src/PWM.hpp
+++ src/PWM.hpp
@@ -12,7 +12,7 @@
  * License as published by the Free Software Foundation; either
  * version 2.1 of the license, or (at your option) any later version.
  *
- * This library is deistributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
