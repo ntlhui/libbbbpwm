2f9a1f7ec102dd59469a05ed66af8647f8ad5093
Nathan Hui <ntlhui@gmail.com>
2014-10-06 19:08:36 -0700

    Modified test for testing purposes

--- test/test.cpp
+++ test/test.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main(int argc, char** argv){
 	PWM testPwm (PWM_P8_13, 50);
-	testPwm.setOnTime(1000000);
+	testPwm.setOnTime(500000);
 	testPwm.setPolarity(PWM_PULSE_HIGH);
-	uint32_t onTime = 1000000;
+	uint32_t onTime = 500000;
 	while(true){
 		cin >> onTime;
 		testPwm.setOnTime(onTime);
